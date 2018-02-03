7b9769777639023cc907edd263a7e43f6a8a1d47
Scott Bender <scott@scottbender.net>
2018-02-03 14:07:39 -0500

    chore: fix copyright notice

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Teppo Kurki <teppo.kurki@ıki.fi>
+ * Copyright 2018 Scott Bender <scott@scottbender.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
